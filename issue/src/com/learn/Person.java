79588170b862e30086ab7754c13a1bb7846ad423
jmkeynes <1442384864@qq.com>
2020-05-09 08:31:55 +0800

    面试遇到的ThreadLocal线程隔离及线程有序输出的问题解决的一种形式

---
+++ issue/src/com/learn/Person.java
@@ -0,0 +1,38 @@
+package com.learn;
+
+/**
+ * @author：江文谱
+ * @date： 2020/5/9 7:38
+ * @version： 1.0
+ */
+public class Person {
+
+    private ThreadLocal<Integer> local = new ThreadLocal<>();
+    private String name;
+    private int seckillQualification;
+    private Integer count;
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getSeckillQualification() {
+        return this.local.get();
+    }
+
+    public void setSeckillQualification(int seckillQualification) {
+        this.local.set(seckillQualification);
+    }
+}
---
+++ issue/src/com/learn/ThreadLoaclDemo.java
@@ -0,0 +1,38 @@
+package com.learn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author：江文谱
+ * @date： 2020/5/9 6:59
+ * @version： 1.0
+ */
+public class ThreadLoaclDemo {
+
+
+    public static void main(String[] args) {
+
+        List<Person> people = new ArrayList<>();
+//        Set<Person> people = new HashSet<>();
+        int count = 0;
+        while (count < 20) {
+            Person p = new Person();
+            int finalCount = count;
+            Thread thread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    p.setName(Thread.currentThread().getName() + "的数据");
+                    p.setCount(finalCount);
+
+                }
+            });
+            thread.setName("线程" + count);
+            thread.start();
+            count++;
+            people.add(p);
+        }
+        people.forEach(person ->
+                System.out.println(person.getName() + "-->" + person.getCount()));
+    }
+}
