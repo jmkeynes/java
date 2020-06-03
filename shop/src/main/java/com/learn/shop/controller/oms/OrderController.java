ac3ea350a23fd0790ae6902b076060e2261c2edd
jmkeynes <1442384864@qq.com>
2020-06-03 23:13:37 +0800

    订单设置更新

--- shop/src/main/java/com/learn/shop/controller/oms/OrderController.java
+++ shop/src/main/java/com/learn/shop/controller/oms/OrderController.java
@@ -3,16 +3,19 @@ package com.learn.shop.controller.oms;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.learn.shop.dto.oms.OrderQueryParam;
+import com.learn.shop.entity.oms.OrderEntity;
 import com.learn.shop.pojo.result.ResultBean;
 import com.learn.shop.service.oms.IOrderService;
 import com.learn.shop.vo.oms.OrderListVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -36,5 +39,22 @@ public class OrderController {
         return this.orderService.getPageOrderList(param);
     }
 
+    @ApiOperation("删除一条订单")
+    @DeleteMapping("/deleteOrderById")
+    public ResultBean<Boolean> deleteOrderById(Long id){
+        return ResultBean.success(this.orderService.removeById(id));
+    }
+
+    @PostMapping("/batchUpdateOrder")
+    @ApiOperation("批量更新")
+    public ResultBean<Boolean> batchUpdateOrder(List<OrderEntity> entitys){
+        return ResultBean.success(this.orderService.updateBatchById(entitys));
+    }
+
+    @PostMapping("/batchDeleteOrder")
+    @ApiOperation("批量删除")
+    public ResultBean<Boolean> batchDeleteOrder(List<OrderEntity> entitys){
+        return ResultBean.success(this.orderService.removeByIds(entitys));
+    }
 }
 
--- shop/src/main/java/com/learn/shop/controller/oms/OrderSettingController.java
+++ shop/src/main/java/com/learn/shop/controller/oms/OrderSettingController.java
@@ -1,9 +1,16 @@
 package com.learn.shop.controller.oms;
 
 
+import com.learn.shop.entity.oms.OrderSettingEntity;
+import com.learn.shop.pojo.result.ResultBean;
+import com.learn.shop.service.oms.IOrderSettingService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.stereotype.Controller;
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -13,9 +20,19 @@ import org.springframework.stereotype.Controller;
  * @author 江文谱
  * @since 2020-06-01
  */
-@Controller
+@Api(tags = "订单设置表 前端控制器")
+@RestController
 @RequestMapping("/oms-order-setting")
 public class OrderSettingController {
+
+    @Resource
+    private IOrderSettingService orderSettingService;
+
+    @ApiOperation("订单设置更新")
+    @PostMapping("/updateOrderSetting")
+    public ResultBean<Boolean> updateOrderSetting(OrderSettingEntity entity){
+        return ResultBean.success(this.orderSettingService.updateById(entity));
+    }
 
 }
 
