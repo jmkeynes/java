ade8a1ce9bfa98df3741cc0caee3f02de79484a2
jmkeynes <1442384864@qq.com>
2020-06-06 10:25:58 +0800

    释放swagger文档资源

---
+++ shop/src/main/java/com/learn/shop/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.learn.shop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author 江文谱
+ * @version 1.0
+ * @date 2020/6/6 10:14
+ * @ClassName WebConfig
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
+        // 解决 SWAGGER 404报错
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+}
