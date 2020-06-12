0d8d86f6cd3aecd4d043ffc97c40fef992c7685e
jmkeynes <1442384864@qq.com>
2020-06-12 10:59:18 +0800

    hashmap中的hashcode的学习

---
+++ learn/src/main/java/com/learn/hash/HashMain.java
@@ -0,0 +1,17 @@
+package com.learn.hash;
+
+/**
+ * @author 江文谱
+ * @version 1.0
+ * @date 2020/6/11 10:00
+ * @ClassName HashMain
+ */
+public class HashMain {
+    public static void main(String[] args) {
+        String k = "k";
+        //hash默认0
+        System.out.println(k.hashCode());
+        Integer i = 4122553;
+        System.out.println(i.hashCode());
+    }
+}
