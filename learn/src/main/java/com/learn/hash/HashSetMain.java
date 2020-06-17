f9cb0e92f05732f74722eb6ef9660a3a4c37573a
jmkeynes <1442384864@qq.com>
2020-06-17 20:04:57 +0800

    hashmap,linkedList,HashSet源码观摩

---
+++ learn/src/main/java/com/learn/hash/HashSetMain.java
@@ -0,0 +1,25 @@
+package com.learn.hash;
+
+import java.util.*;
+
+/**
+ * @author 江文谱
+ * @version 1.0
+ * @date 2020/6/17
+ */
+public class HashSetMain {
+
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("111");
+        set.add("111");
+        set.forEach(System.out::println);
+
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.addAll(set);
+        linkedList.add("222");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("k","v");
+    }
+}
