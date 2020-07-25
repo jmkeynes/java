4c146e18e458ae869bc272099e367c8dd05729e4
jmkeynes <1442384864@qq.com>
2020-07-25 17:33:18 +0800

    将之前弄乱的配置文件重新修改回来，已经swagger修改成了knife4j方式的文档模式

--- shop/src/main/java/com/learn/shop/config/WebConfig.java
+++ shop/src/main/java/com/learn/shop/config/WebConfig.java
@@ -18,6 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
         // 解决 SWAGGER 404报错
         registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/doc.html").addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
